47b805a377a0f8810390dcf296265cc5d9882931
varnavi430 <71841456+varnavi430@users.noreply.github.com>
2021-09-22 15:34:01 +0530

    Update MedicineFragmentAdd.java

--- app/src/main/java/lk/drugreminder/ui/medicine/MedicineFragmentAdd.java
+++ app/src/main/java/lk/drugreminder/ui/medicine/MedicineFragmentAdd.java
@@ -14,6 +14,31 @@ import lk.drugreminder.R;
 
 public class MedicineFragmentAdd extends Fragment {
 
+    private LinearLayout lyPills, lyCards, lyBoxes;
+    private LinearLayout lyAddPills;
+    private Button btnAddByPills, btnAddByCards, btnAddByBoxes;
+    private Button btnSavePills, btnUpdatePills;
+    private View view;
 
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        view = inflater.inflate(R.layout.fragment_medicine_add, container, false);
+
+        lyAddPills = view.findViewById(R.id.ly_add_pills);
+
+        lyPills = view.findViewById(R.id.ly_pills);
+        lyCards = view.findViewById(R.id.ly_cards);
+        lyBoxes = view.findViewById(R.id.ly_boxes);
+
+        btnSavePills = view.findViewById(R.id.btn_save_pills);
+        btnUpdatePills = view.findViewById(R.id.btn_update_pills);
+
+        btnAddByPills = view.findViewById(R.id.btn_add_by_pills);
+        btnAddByCards = view.findViewById(R.id.btn_add_by_cards);
+        btnAddByBoxes = view.findViewById(R.id.btn_add_by_box);
+
+        return view;
+    }
 
 }
