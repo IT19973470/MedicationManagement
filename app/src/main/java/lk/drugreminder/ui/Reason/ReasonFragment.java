21b98d2dad5a360235c5b5d63333b59f8a922714
Gayanpoor <71841314+IT20011970@users.noreply.github.com>
2021-09-24 14:59:55 +0530

    insert with all

--- app/src/main/java/lk/drugreminder/ui/Reason/ReasonFragment.java
+++ app/src/main/java/lk/drugreminder/ui/Reason/ReasonFragment.java
@@ -52,7 +52,7 @@ public class ReasonFragment extends Fragment {
 
        // loadSicknesses();
 
-        //return view;
+        return view;
     }
 
     private void addDisease() {
@@ -61,7 +61,7 @@ public class ReasonFragment extends Fragment {
 //        dbDisease.push().setValue(sickness);
         dbDisease.child(id).setValue(sickness);
         txtDisease.setText("");
-        Toast.makeText(getContext(), "Sickness added successfully", Toast.LENGTH_LONG).show();
+        Toast.makeText(getContext(), "Reason added successfully", Toast.LENGTH_LONG).show();
     }
 
 }
