c2ccb9703d84535c935418ffaa33eb1574ed5125
Imalka Gunawardana <imalkagunawardana1@gmail.com>
2021-09-25 18:59:02 +0530

    Create Calculations.java

---
+++ app/src/main/java/lk/drugreminder/calculations/Calculations.java
@@ -0,0 +1,59 @@
+package lk.drugreminder.calculations;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Calculations {
+
+    public static int[] calcNextDueTime(int timeH, int timeM, int hours, int minutes) {
+        int[] time = new int[2];
+        timeM += minutes;
+        if (timeM >= 60) {
+            timeH++;
+            timeM -= 60;
+        }
+
+        timeH += hours;
+
+        if (timeH >= 24) {
+            timeH -= 24;
+        }
+
+        time[0] = timeH;
+        time[1] = timeM;
+
+        return time;
+    }
+
+    public static String pillsEndOn(int pillsCount, int dose, int nextTimeH, int nextTimeM, int intervalH, int intervalM) {
+        try {
+            pillsCount = pillsCount / dose;
+            int days = 0;
+            for (int i = 0; i < pillsCount; i++) {
+                nextTimeM += intervalM;
+                if (nextTimeM >= 60) {
+                    nextTimeH++;
+                    nextTimeM -= 60;
+                }
+
+                nextTimeH += intervalH;
+
+                if (nextTimeH >= 24) {
+                    days++;
+                    nextTimeH -= 24;
+                }
+            }
+
+            LocalDateTime localDateTime = LocalDate.now().atTime(nextTimeH, nextTimeM);
+
+            localDateTime = localDateTime.plusDays(days);
+
+            return localDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm a"));
+        } catch (ArithmeticException e) {
+
+        }
+        return "N/A";
+    }
+
+}
